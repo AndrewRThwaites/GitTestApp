33dc0b81705eeb7d85d2628964cfa69c44386e2e
Andrew Thwaites <andrew.r.thwaites.work@outlook.com>
2022-09-13 14:21:22 +0100

    first commit

---
+++ GittestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GittestApp
+//
+//  Created by Andrew Thwaites on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
